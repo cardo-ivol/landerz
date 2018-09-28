52ac16a03bb053985cd5ee3883af412d212947c3
cardo-ivol <ricardo@ivacationonline.com>
2018-09-28 09:17:40 -0500

    update chart

--- application/views/index.php
+++ application/views/index.php
@@ -174,6 +174,43 @@
 
             <hr>
 
+            <!-- Section: About -->
+            <section id="about" class="section about mt-5 mb-5 py-3 wow fadeIn" data-wow-delay="0.2s">
+
+                <!-- Grid row -->
+                <div class="row pt-2 mt-5">
+
+
+
+                    <!-- Grid column -->
+                    <div class="col-md-12 wow fadeIn" data-wow-delay="0.4s">
+
+                        <!-- Secion heading -->
+                        <h3 class="mb-5 dark-grey-text title font-weight-bold wow fadeIn" data-wow-delay="0.2s">
+                            <strong>The Market</strong>
+                        </h3>
+
+                        <!-- Description -->
+                        <p align="justify" class="grey-text">The dating industry is a $10B plus international market opportunity with consistent three digit revenue growth annually experienced by Tinder, the world's most popular dating software.</p>
+
+
+                    </div>
+                    <!-- Grid column -->
+                </div>
+
+                <div class="row">
+                  <div class="col-md-12 wow fadeIn" data-wow-delay="0.6s">
+                    <canvas id="lineChart"></canvas>
+                  </div>
+
+                </div>
+                <!-- Grid row -->
+
+            </section>
+            <!-- Section: About -->
+
+            <hr>
+
             <!-- Section: Features v.4 -->
             <section id="features" class="section feature-box mb-5 pb-4">
 
@@ -915,6 +952,43 @@
                 }, 300);
             });
         });
+    </script>
+
+    <script>
+    //line
+    var ctxL = document.getElementById("lineChart").getContext('2d');
+    var myLineChart = new Chart(ctxL, {
+      type: 'line',
+      data: {
+        labels: ["January", "February", "March", "April", "May", "June", "July"],
+        datasets: [{
+            label: "My First dataset",
+            data: [65, 59, 80, 81, 56, 55, 40],
+            backgroundColor: [
+              'rgba(105, 0, 132, .2)',
+            ],
+            borderColor: [
+              'rgba(200, 99, 132, .7)',
+            ],
+            borderWidth: 2
+          },
+          {
+            label: "My Second dataset",
+            data: [28, 48, 40, 19, 86, 27, 90],
+            backgroundColor: [
+              'rgba(0, 137, 132, .2)',
+            ],
+            borderColor: [
+              'rgba(0, 10, 130, .7)',
+            ],
+            borderWidth: 2
+          }
+        ]
+      },
+      options: {
+        responsive: true
+      }
+    });
     </script>
 </body>
 
