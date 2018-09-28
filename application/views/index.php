b346d8ab869a6d97a9640ad6b4a76cb3ef4cfe55
Ricardo D Ramirez <ricardo@ivacationline.com>
2018-09-28 14:49:37 -0500

    update

--- application/views/index.php
+++ application/views/index.php
@@ -996,7 +996,9 @@
     $('.productList').slick({
       infinite: true,
       slidesToShow: 3,
-      slidesToScroll: 3
+      slidesToScroll: 3,
+      autoplay: true,
+      autoplaySpeed: 2000,
     });
     </script>
 
