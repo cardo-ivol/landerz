08edd3bd5bd101028e7f8a4affc29cd394781860
cardo-ivol <ricardo@ivacationonline.com>
2018-09-28 10:58:10 -0500

    update

--- application/views/index.php
+++ application/views/index.php
@@ -337,6 +337,166 @@
 
           <div class="container">
 
+            <!-- Section: About -->
+            <section id="about" class="section about mt-5 mb-5 py-3 wow fadeIn" data-wow-delay="0.2s">
+
+                <!-- Grid row -->
+                <div class="row pt-2 mt-5">
+
+
+
+                    <!-- Grid column -->
+                    <div class="col-md-12 wow fadeIn" data-wow-delay="0.4s">
+
+                        <!-- Secion heading -->
+                        <h3 class="mb-5 dark-grey-text title font-weight-bold text-center wow fadeIn" data-wow-delay="0.2s">
+                            <strong>Investor Highlights</strong>
+                        </h3>
+                        <h4 class="mb-5 dark-grey-text title font-weight-bold text-center wow fadeIn" data-wow-delay="0.4s">
+                          Minimum Investment: $40,000
+                        </h4>
+                        <!-- Description -->
+                        <!-- <p align="justify" class="grey-text"><em>syftr</em> and its sister Apps (face2face, dskreat, L4L) provide users with tools to confirm accurate identification, incorporating innovative timestamped profile facial pictures and video chats for confirmation. DYZN, LLC has created a revolutionary infrastructure (template) currently incorporated in syftr, f2f, dskreat and L4L that can be replicated and deployed seamlessly into new niche Apps to meet the needs of other demographics. (veterans, seniors, pets, etc)</p> -->
+
+
+                    </div>
+                    <!-- Grid column -->
+                </div>
+
+
+                <!-- Grid row -->
+
+            </section>
+            <!-- Section: About -->
+
+            <hr>
+
+            <hr class="mt-5">
+
+            <!-- Section: Team v.1 -->
+            <section class="section team-section pb-5" id="team">
+
+                <!-- Section heading -->
+                <h3 class="text-center title my-5 dark-grey-text font-weight-bold wow fadeIn" data-wow-delay="0.2s">
+                    <strong>Our amazing team</strong>
+                </h3>
+                <!-- Section description -->
+                <p class="text-center w-responsive mx-auto my-5 grey-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit, error amet numquam iure provident voluptate
+                    esse quasi, veritatis totam voluptas nostrum quisquam eum porro a pariatur accusamus veniam.</p>
+
+                <!-- Grid row -->
+                <div class="row text-center">
+
+                    <!-- Grid column -->
+                    <div class="col-lg-3 col-md-6 mb-4">
+                        <div class="card card-body">
+                            <div class="avatar mx-auto mt-3 mb-3">
+                                <img src="https://mdbootstrap.com/img/Photos/Avatars/img%20(25).jpg" class="rounded-circle" alt="First sample avatar image">
+                            </div>
+                            <h6 class="font-weight-bold title">Anna Deynah</h6>
+
+                            <p class="font-small grey-text">Graphic designer</p>
+
+                            <ul class="list-unstyled">
+                                <!-- Facebook -->
+                                <a class="p-2 m-2 fa-lg fb-ic">
+                                    <i class="fa fa-facebook orange-text"> </i>
+                                </a>
+                                <!-- Twitter -->
+                                <a class="p-2 m-2 fa-lg tw-ic">
+                                    <i class="fa fa-twitter orange-text"> </i>
+                                </a>
+                                <!-- Instagram -->
+                                <a class="p-2 m-2 fa-lg ins-ic">
+                                    <i class="fa fa-instagram orange-text"> </i>
+                                </a>
+                            </ul>
+                        </div>
+                    </div>
+                    <!-- Grid column -->
+
+                    <!-- Grid column -->
+                    <div class="col-lg-3 col-md-6 mb-4">
+                        <div class="card card-body">
+                            <div class="avatar mx-auto mt-3 mb-3">
+                                <img src="https://mdbootstrap.com/img/Photos/Avatars/img%20(9).jpg" class="rounded-circle" alt="First sample avatar image">
+                            </div>
+                            <h6 class="font-weight-bold title">John Doe</h6>
+
+                            <p class="font-small grey-text">Web developer</p>
+
+                            <ul class="list-unstyled">
+                                <!-- Facebook -->
+                                <a class="p-2 m-2 fa-lg fb-ic">
+                                    <i class="fa fa-facebook orange-text"> </i>
+                                </a>
+                                <!-- Instagram -->
+                                <a class="p-2 m-2 fa-lg ins-ic">
+                                    <i class="fa fa-instagram orange-text"> </i>
+                                </a>
+                            </ul>
+                        </div>
+                    </div>
+                    <!-- Grid column -->
+
+                    <!-- Grid column -->
+                    <div class="col-lg-3 col-md-6 mb-4">
+                        <div class="card card-body">
+                            <div class="avatar mx-auto mt-3 mb-3">
+                                <img src="https://mdbootstrap.com/img/Photos/Avatars/img%20(5).jpg" class="rounded-circle" alt="First sample avatar image">
+                            </div>
+                            <h6 class="font-weight-bold title">Maria Smith</h6>
+
+                            <p class="font-small grey-text">Photographer</p>
+
+                            <ul class="list-unstyled">
+                                <!-- Facebook -->
+                                <a class="p-2 m-2 fa-lg fb-ic">
+                                    <i class="fa fa-facebook orange-text"> </i>
+                                </a>
+                                <!-- Instagram -->
+                                <a class="p-2 m-2 fa-lg ins-ic">
+                                    <i class="fa fa-instagram orange-text"> </i>
+                                </a>
+                                <!-- Dribbble -->
+                                <a class="p-2 m-2 fa-lg ins-ic">
+                                    <i class="fa fa-dribbble orange-text"> </i>
+                                </a>
+                            </ul>
+                        </div>
+                    </div>
+                    <!-- Grid column -->
+
+                    <!-- Grid column -->
+                    <div class="col-lg-3 col-md-6 mb-4">
+                        <div class="card card-body">
+                            <div class="avatar mx-auto mt-3 mb-3">
+                                <img src="https://mdbootstrap.com/img/Photos/Avatars/img%20(8).jpg" class="rounded-circle" alt="First sample avatar image">
+                            </div>
+                            <h6 class="font-weight-bold title">Tom Adams</h6>
+
+                            <p class="font-small grey-text">Backend developer</p>
+
+                            <ul class="list-unstyled">
+                                <!-- Facebook -->
+                                <a class="p-2 m-2 fa-lg fb-ic">
+                                    <i class="fa fa-facebook orange-text"> </i>
+                                </a>
+                                <!-- Github -->
+                                <a class="p-2 m-2 fa-lg ins-ic">
+                                    <i class="fa fa-github orange-text"> </i>
+                                </a>
+                            </ul>
+                        </div>
+                    </div>
+                    <!-- Grid column -->
+
+                </div>
+                <!-- Grid row -->
+
+            </section>
+            <!-- Section: Team v.1 -->
+
             <!-- Section: Features v.4 -->
             <section id="features" class="section feature-box mb-5 pb-4">
 
