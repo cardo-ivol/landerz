672b9ccb2b861fb655db4a65a7b8686367c212a9
cardo-ivol <ricardo@ivacationonline.com>
2018-09-28 09:20:40 -0500

    update graph

--- application/views/index.php
+++ application/views/index.php
@@ -965,21 +965,10 @@
             label: "My First dataset",
             data: [65, 59, 80, 81, 56, 55, 40],
             backgroundColor: [
-              'rgba(105, 0, 132, .2)',
+              'rgba(158, 158, 158, .2)',
             ],
             borderColor: [
-              'rgba(200, 99, 132, .7)',
-            ],
-            borderWidth: 2
-          },
-          {
-            label: "My Second dataset",
-            data: [28, 48, 40, 19, 86, 27, 90],
-            backgroundColor: [
-              'rgba(0, 137, 132, .2)',
-            ],
-            borderColor: [
-              'rgba(0, 10, 130, .7)',
+              'rgba(0, 0, 0, .7)',
             ],
             borderWidth: 2
           }
