3cee0f20a580886da58a55abfbfb2ed2518257f1
Ricardo D Ramirez <ricardo@ivacationline.com>
2018-09-28 15:43:28 -0500

    update

--- application/views/index.php
+++ application/views/index.php
@@ -1082,7 +1082,7 @@
     var myChart = new Chart(ctx, {
         type: 'bar',
         data: {
-            labels: ["False idenity (Catfished)", "Safety Concerns", "inappropriate images"],
+            labels: ["Catfished", "Safety Concerns", "Inappropriate Images"],
             datasets: [{
                 label: '% of Users',
                 data: [34, 29, 26],
