5cef85bacc8bc1913197bbf663293b864ba45b44
cardo-ivol <ricardo@ivacationonline.com>
2018-09-28 08:58:02 -0500

    update

--- application/views/index.php
+++ application/views/index.php
@@ -110,12 +110,10 @@
                         <div class="col-md-12 col-lg-6 text-center text-md-left margins">
                             <div class="dark-grey-text">
                                 <h1 class="display-4 title mt-md-5 mt-lg-0 font-weight-bold wow fadeIn" data-wow-delay="0.3s">
-                                    <strong>Creative agency</strong>
+                                    <strong>DYZN, LLC</strong>
                                 </h1>
                                 <hr class="hr-light wow fadeIn" data-wow-delay="0.3s">
-                                <h6 class="grey-text wow fadeIn" data-wow-delay="0.3s">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem repellendus quasi fuga nesciunt
-                                    dolorum nulla magnam veniam sapiente, fugiat! Commodi sequi non animi ea dolor molestiae
-                                    iste.
+                                <h6 class="grey-text wow fadeIn" data-wow-delay="0.3s">"Innovative connections to meaningful encounters"
                                 </h6>
                                 <br>
                                 <a class="btn btn-white btn-rounded blue-text font-weight-bold ml-lg-0 wow fadeIn" data-wow-delay="0.3s">Portfolio</a>
