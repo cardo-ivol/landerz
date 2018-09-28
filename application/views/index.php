ebbecf16c28494dad916e96692abbc8ea92a5122
cardo-ivol <ricardo@ivacationonline.com>
2018-09-28 08:51:38 -0500

    update

--- application/views/index.php
+++ application/views/index.php
@@ -103,7 +103,7 @@
         </nav>
 
         <!--Intro Section-->
-        <section class="view intro-2" style="background-image: url('https://mdbootstrap.com/img/Photos/Others/images/62.jpg'); background-repeat: no-repeat; background-size: cover; background-position: center center;">
+        <section class="view intro-2" style="background-image: url('/public/img/bg.jpg'); background-repeat: no-repeat; background-size: cover; background-position: center center;">
             <div class="mask">
                 <div class="container h-100 d-flex justify-content-center align-items-center">
                     <div class="row flex-center pt-5 mt-3">
