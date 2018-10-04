228a1c138107af1b613e79c4856c9c42ac0cd1bf
cardo-ivol <ricardo@ivacationonline.com>
2018-10-04 11:12:54 -0500

    updates

--- application/views/index.php
+++ application/views/index.php
@@ -110,7 +110,7 @@
         }
 
         .card-adj {
-          padding: 4.25rem !important; 
+          padding: 4.25rem !important;
         }
     </style>
 </head>
@@ -418,6 +418,8 @@
                               <strong>The Opportunity</strong>
                           </h3>
 
+                        </div>
+
 
                               <!--Panel-->
 
@@ -426,19 +428,7 @@
                                       proident. Ad vegan excepteur butcher vice lomo leggings.
 
                                   </p> -->
-                                  <div class="mx-4 my-4">
-                                    <ul>
-                                      <li>DYZN, LLC has initiated it's first round private investment offering. This round of funding has a target raise of $1.6M by December 1, 2018</li>
-                                      <li>Funding will launch syftr's ad campaign</li>
-                                      <li>Complete and publish the Android syftr edition, (iOS/Apple version is complete)</li>
-                                      <li>Complete the development of DYZN WebRTC (video chat) solution</li>
-                                      <li>face2face projected launch late 2018</li>
-                                      <li>dskreat and L4L projected launch early 2019</li>
-                                    </ul>
-                                  <!-- <p align="justify" class="grey-text">DYZN, LLC has initiated it's first round private investment offering. This round of funding has a target raise of $1.6M by December 1, 2018.</p>
-                                  <p align="justify" class="grey-text">Funding will launch syftr's ad campaign, complete and publish the Android syftr edition, (iOS/Apple version is complete) and complete the development of DYZN WebRTC (video chat) solution.</p>
-                                  <p align="justify" class="grey-text">face2face, dskreat and L4L Apps will follow in succession with projected launches throughout 2019.</p> -->
-                                </div>
+
 
                               <!--/.Panel-->
 
@@ -451,7 +441,22 @@
 
                   <!-- Vertical Steppers -->
                   <div class="row mt-1">
-                      <div class="col-md-12">
+                    <div class="col-md-6">
+                      <div class="mx-4 my-4">
+                        <ul>
+                          <li>DYZN, LLC has initiated it's first round private investment offering. This round of funding has a target raise of $1.6M by December 1, 2018</li>
+                          <li>Funding will launch syftr's ad campaign</li>
+                          <li>Complete and publish the Android syftr edition, (iOS/Apple version is complete)</li>
+                          <li>Complete the development of DYZN WebRTC (video chat) solution</li>
+                          <li>face2face projected launch late 2018</li>
+                          <li>dskreat and L4L projected launch early 2019</li>
+                        </ul>
+                      <!-- <p align="justify" class="grey-text">DYZN, LLC has initiated it's first round private investment offering. This round of funding has a target raise of $1.6M by December 1, 2018.</p>
+                      <p align="justify" class="grey-text">Funding will launch syftr's ad campaign, complete and publish the Android syftr edition, (iOS/Apple version is complete) and complete the development of DYZN WebRTC (video chat) solution.</p>
+                      <p align="justify" class="grey-text">face2face, dskreat and L4L Apps will follow in succession with projected launches throughout 2019.</p> -->
+                    </div>
+                  </div>
+                      <div class="col-md-6">
 
                           <!-- Stepers Wrapper -->
                           <ul class="stepper stepper-vertical">
