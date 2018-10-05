7b71fa4259b1e64382c1c779d68932b8b501fc38
cardo-ivol <ricardo@ivacationonline.com>
2018-10-05 17:03:09 -0500

    update

--- application/views/index.php
+++ application/views/index.php
@@ -399,16 +399,16 @@
                   </div>
 
                   <div class="row pt-2 mt-5">
-                    <div class="col-md-3 text-center wow slideInUp" data-wow-delay="0.6s">
+                    <div class="col-md-3 text-center wow slideInUp mb-3" data-wow-delay="0.6s">
                       <img src="/public/img/iphonex_f2f.png" class="img-fluid" style="max-width: 175px;">
                     </div>
-                    <div class="col-md-3 text-center wow slideInUp" data-wow-delay="0.6s">
+                    <div class="col-md-3 text-center wow slideInUp mb-3" data-wow-delay="0.6s">
                       <img src="/public/img/iphonex_dskreat.png" class="img-fluid" style="max-width: 175px;">
                     </div>
-                    <div class="col-md-3 text-center wow slideInUp" data-wow-delay="0.6s">
+                    <div class="col-md-3 text-center wow slideInUp mb-3" data-wow-delay="0.6s">
                       <img src="/public/img/iphonex_l4l.png" class="img-fluid" style="max-width: 175px;">
                     </div>
-                    <div class="col-md-3 text-center wow slideInUp" data-wow-delay="0.6s">
+                    <div class="col-md-3 text-center wow slideInUp mb-3" data-wow-delay="0.6s">
                       <img src="/public/img/iphonex_syftr.png" class="img-fluid" style="max-width: 175px;">
                     </div>
 
